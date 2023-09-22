dc35063a41caec8a1d83a5244608dca3fbc33ca3
Marco Zellini <zellinimarco@gmail.com>
2023-09-22 15:04:28 +0200

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+
+export default {
+  name: 'App'
+
+
+}
+</script>
+
+<template>
+  <h1>Titolo</h1>
+</template>
+
+<style lang="scss" scoped>
+@use '../src/assets/scss/partials/variables' as *;
+</style>

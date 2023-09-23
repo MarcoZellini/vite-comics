11f95ea85b473b90de4e5043a72af0ba58be9f0d
Marco Zellini <zellinimarco@gmail.com>
2023-09-23 11:52:17 +0200

    Add FooterLinks Component

---
+++ src/components/FooterLinks.vue
@@ -0,0 +1,193 @@
+<script>
+
+export default {
+    name: 'FooterLinks',
+    data() {
+        return {
+            comicsMenu: {
+                name: 'Dc Comics',
+                linkList: [
+                    {
+                        name: 'Characters',
+                        url: '#'
+                    },
+                    {
+                        name: 'Comics',
+                        url: '#'
+                    },
+                    {
+                        name: 'Movies',
+                        url: '#'
+                    },
+                    {
+                        name: 'TV',
+                        url: '#'
+                    },
+                    {
+                        name: 'Games',
+                        url: '#'
+                    },
+                    {
+                        name: 'Videos',
+                        url: '#'
+                    },
+                    {
+                        name: 'News',
+                        url: '#'
+                    },
+                ]
+            },
+            shopMenu: {
+                name: 'Shop',
+                linkList: [
+                    {
+                        name: 'Shop DC',
+                        url: '#'
+                    },
+                    {
+                        name: 'Shop DC Collectibles',
+                        url: '#'
+                    }
+                ]
+            },
+            dcMenu: {
+                name: 'DC',
+                linkList: [
+                    {
+                        name: 'Terms Of Use',
+                        url: '#'
+                    },
+                    {
+                        name: 'Privacy Policy (New)',
+                        url: '#'
+                    },
+                    {
+                        name: 'Ad Choices',
+                        url: '#'
+                    },
+                    {
+                        name: 'Advertising',
+                        url: '#'
+                    },
+                    {
+                        name: 'Job',
+                        url: '#'
+                    },
+                    {
+                        name: 'Subscriptions',
+                        url: '#'
+                    },
+                    {
+                        name: 'Talent Workshops',
+                        url: '#'
+                    },
+                    {
+                        name: 'CPSC Certificates',
+                        url: '#'
+                    },
+                    {
+                        name: 'Ratings',
+                        url: '#'
+                    },
+                    {
+                        name: 'Shop Help',
+                        url: '#'
+                    },
+                    {
+                        name: 'Contact Us',
+                        url: '#'
+                    }
+                ]
+            },
+            sitesMenu: {
+                name: 'Sites',
+                linkList: [
+                    {
+                        name: 'DC',
+                        url: '#'
+                    },
+                    {
+                        name: 'MAD Megazine',
+                        url: '#'
+                    },
+                    {
+                        name: 'DC Kids',
+                        url: '#'
+                    },
+                    {
+                        name: 'DC Universe',
+                        url: '#'
+                    },
+                    {
+                        name: 'DC Power Visa',
+                        url: '#'
+                    }
+                ]
+            }
+        }
+    }
+}
+
+</script>
+
+<template>
+    <div id="useful_links" class="row text-light">
+
+        <div class="row">
+            <div class=" col-4">
+                <ul class="list-unstyled">
+                    <h5 class="text-uppercase">{{ comicsMenu.name }}</h5>
+                    <li v-for="item in comicsMenu.linkList">
+                        <a :href="item.url" class="text-decoration-none text-light">{{ item.name }}</a>
+                    </li>
+                </ul>
+
+                <ul class="list-unstyled mt-5">
+                    <h5 class="text-uppercase">{{ shopMenu.name }}</h5>
+                    <li v-for="item in shopMenu.linkList">
+                        <a :href="item.url" class="text-decoration-none text-light">{{ item.name }}</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.col-4 -->
+            <div class="col-4">
+                <ul class="list-unstyled">
+                    <h5 class="text-uppercase">{{ dcMenu.name }}</h5>
+                    <li v-for="item in dcMenu.linkList">
+                        <a :href="item.url" class="text-decoration-none text-light">{{ item.name }}</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.col-4 -->
+            <div class="col-4">
+                <ul class="list-unstyled">
+                    <h5 class="text-uppercase">{{ sitesMenu.name }}</h5>
+                    <li v-for="item in sitesMenu.linkList">
+                        <a :href="item.url" class="text-decoration-none text-light">{{ item.name }}</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.col-4 -->
+        </div>
+
+
+
+
+    </div>
+</template>
+
+<style lang="scss" scoped>
+#useful_links {
+    .col-4 {
+        ul.list-unstyled {
+            h5 {
+                font-size: 15px;
+            }
+
+            li a {
+                font-size: 10px;
+            }
+        }
+    }
+}
+</style>
--- src/components/FooterTop.vue
+++ src/components/FooterTop.vue
@@ -1,81 +1,35 @@
 <script>
+import FooterLinks from './FooterLinks.vue'
+
 export default {
-    name: 'FooterTop'
+    name: 'FooterTop',
+    components: {
+        FooterLinks
+    }
 }
 </script>
 
 <template>
-    <section id="useful_links">
+    <div id="footer_top">
         <div class="container py-5">
-            <div class="row justify-content-between">
+            <div class="row align-items-center">
                 <div class="col-7">
-                    <div class="row">
-                        <div class="col-4 text-light">
-                            <h5 class="text-uppercase">Dc Comics</h5>
-                            <ul class="list-unstyled">
-                                <li>Characters</li>
-                                <li>Comics</li>
-                                <li>Movies</li>
-                                <li>TV</li>
-                                <li>Games</li>
-                                <li>Videos</li>
-                                <li>News</li>
-                            </ul>
-                            <h5 class="text-uppercase">Shop</h5>
-                            <ul class="list-unstyled">
-                                <li>Shop Dc</li>
-                                <li>Shop Dc Collectibles</li>
-                            </ul>
-                        </div>
-                        <!-- /.col -->
-                        <div class="col-4 text-light">
-                            <h5 class="text-uppercase">Dc</h5>
-                            <ul class="list-unstyled">
-                                <li>Characters</li>
-                                <li>Comics</li>
-                                <li>Movies</li>
-                                <li>TV</li>
-                                <li>Games</li>
-                                <li>Videos</li>
-                                <li>News</li>
-                                <li>Games</li>
-                                <li>Videos</li>
-                                <li>News</li>
-                                <li>News</li>
-                            </ul>
-                        </div>
-                        <!-- /.col -->
-                        <div class="col-4 text-light">
-                            <h5 class="text-uppercase">Sites</h5>
-                            <ul class="list-unstyled">
-                                <li>Characters</li>
-                                <li>Comics</li>
-                                <li>Movies</li>
-                                <li>TV</li>
-                                <li>Games</li>
-                                <li>Videos</li>
-                                <li>News</li>
-                            </ul>
-                        </div>
-                        <!-- /.col -->
+                    <FooterLinks />
+                </div>
+                <!-- /.col-7 -->
+                <div class="col-5">
+                    <div class="position-relative d-flex justify-content-center align-items-center">
+                        <img class="img-fluid position-absolute" src="..\assets\img\dc-logo-bg.png" alt="">
                     </div>
                 </div>
-                <!-- /.col-6 -->
-                <div class="col-5 position-relative d-flex justify-content-center align-items-center">
-                    <img class="img-fluid position-absolute" src="..\assets\img\dc-logo-bg.png" alt="">
-                </div>
-                <!-- /.col-6 -->
+                <!-- /.col-5 -->
             </div>
-            <!-- /.row -->
-
         </div>
-        <!-- /.container -->
-    </section>
-    <!-- /#useful_links -->
+    </div>
 </template>
 
 <style>
-#useful_links {
+#footer_top {
     background-image: url(../assets/img/footer-bg.jpg);
     background-repeat: no-repeat;
     background-size: cover;

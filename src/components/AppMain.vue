9947c9c33d4a1104db8fe5f1996c865c43467ad8
Marco Zellini <zellinimarco@gmail.com>
2023-09-23 10:49:20 +0200

    Add MainMenu as a Component

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,6 +1,11 @@
 <script>
+import MainMenu from './MainMenu.vue'
+
 export default {
-    name: 'AppMain'
+    name: 'AppMain',
+    components: {
+        MainMenu
+    }
 }
 </script>
 
@@ -15,56 +20,7 @@ export default {
         </section>
         <!-- /#content -->
 
-        <section id="main_menu">
-            <div class="container">
-                <ul class="list-unstyled d-flex justify-content-around align-items-center py-5 m-0">
-                    <li class="d-flex align-items-center p-3">
-                        <a href="#" class="d-flex align-items-center">
-                            <div class="item_img">
-                                <img class="img-fluid" src="../assets/img/buy-comics-digital-comics.png" alt="">
-                            </div>
-                            <span>Digital Comics</span>
-                        </a>
-                    </li>
-                    <li class="d-flex align-items-center p-3">
-                        <a href="#" class="d-flex align-items-center">
-                            <div class="item_img">
-                                <img class="img-fluid" src="../assets/img/buy-comics-merchandise.png" alt="">
-                            </div>
-                            <span>DC Merchandise</span>
-                        </a>
-                    </li>
-                    <li class="d-flex align-items-center p-3">
-                        <a href="#" class="d-flex align-items-center">
-                            <div class="item_img">
-                                <img class="img-fluid" src="../assets/img/buy-comics-subscriptions.png" alt="">
-                            </div>
-                            <span>Subscription</span>
-                        </a>
-                    </li>
-                    <li class="d-flex align-items-center p-3">
-                        <a href="#" class="d-flex align-items-center">
-                            <div class="item_img">
-                                <img class="img-fluid" src="../assets/img/buy-comics-shop-locator.png" alt="">
-                            </div>
-                            <span>Comic Shop Locator</span>
-                        </a>
-                    </li>
-                    <li class="d-flex align-items-center p-3">
-                        <a href="#" class="d-flex align-items-center">
-                            <div class="item_img">
-                                <img class="img-fluid visa_card" src="../assets/img/buy-dc-power-visa.svg" alt="">
-                            </div>
-                            <span>DC Power Visa</span>
-                        </a>
-                    </li>
-
-
-                </ul>
-            </div>
-            <!-- /.container -->
-        </section>
-        <!-- /#main_menu -->
+        <MainMenu />
 
     </main>
     <!-- /#app_main -->
@@ -85,24 +41,5 @@ export default {
             margin: 0;
         }
     }
-
-    #main_menu {
-        background-color: $dc_primary;
-
-        li>a {
-            color: $dc_light;
-            text-decoration: none;
-            text-transform: uppercase;
-
-            .item_img img {
-                max-height: 60px;
-                margin-right: 1rem;
-            }
-        }
-    }
-}
-
-.visa_card {
-    height: 50px;
 }
 </style>
---
+++ src/components/MainMenu.vue
@@ -0,0 +1,95 @@
+<script>
+
+export default {
+    name: 'MainMenu',
+    data() {
+        return {
+            menuItems: [
+                {
+                    url: '#',
+                    imgPath: 'src/assets/img/buy-comics-digital-comics.png', /* Perche????  percorsi dell' immagine nello stesso punto ma devono essere scritti diversamente */
+                    name: 'Digital Comics'
+                },
+                {
+                    url: '#',
+                    imgPath: 'src/assets/img/buy-comics-merchandise.png',
+                    name: 'DC Merchandise'
+                },
+                {
+                    url: '#',
+                    imgPath: 'src/assets/img/buy-comics-subscriptions.png',
+                    name: 'Subscription'
+                },
+                {
+                    url: '#',
+                    imgPath: 'src/assets/img/buy-comics-shop-locator.png',
+                    name: 'Comic Shop Locator'
+                },
+                {
+                    url: '#',
+                    imgPath: 'src/assets/img/buy-dc-power-visa.svg',
+                    name: 'DC Power Visa'
+                },
+            ]
+        }
+    }
+}
+
+</script>
+
+<template>
+    <section id="main_menu">
+        <div class="container">
+            <ul class="list-unstyled d-flex justify-content-around align-items-center py-5 m-0">
+
+
+                <li v-for="item in menuItems" class="d-flex align-items-center p-3">
+                    <a :href="item.url" class="d-flex align-items-center">
+                        <div class="item_img">
+                            <img class="img-fluid" :class="item.imgPath.includes('visa') ? 'visa_card' : ''"
+                                :src="item.imgPath" alt="">
+                        </div>
+                        <span>{{ item.name }}</span>
+                    </a>
+                </li>
+
+                <!-- Perche????  percorsi dell' immagine nello stesso punto ma devono essere scritti diversamente-->
+
+                <!-- <li class="d-flex align-items-center p-3">
+                    <a href="#" class="d-flex align-items-center">
+                        <div class="item_img">
+                            <img class="img-fluid" src="../assets/img/buy-comics-merchandise.png" alt="">
+                        </div>
+                        <span>DC Merchandise</span>
+                    </a>
+                </li> -->
+
+            </ul>
+        </div>
+        <!-- /.container -->
+    </section>
+    <!-- /#main_menu -->
+</template>
+
+<style lang="scss" scoped>
+@use '../../src/assets/scss/partials/variables' as *;
+
+#main_menu {
+    background-color: $dc_primary;
+
+    li>a {
+        color: $dc_light;
+        text-decoration: none;
+        text-transform: uppercase;
+
+        .item_img img {
+            max-height: 60px;
+            margin-right: 1rem;
+        }
+    }
+}
+
+.visa_card {
+    height: 50px;
+}
+</style>
